c00940d3407959fa6684684151dee1cab5c72e76
tim48-robot <justintimothy303@gmail.com>
2025-09-08 19:26:47 +0700

    chores: TERNYATA HARUS TUGAS_INDIVIDU YAALLAH

--- football_project/settings.py
+++ football_project/settings.py
@@ -135,7 +135,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images) ## 
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
