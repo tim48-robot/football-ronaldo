b42945a33e715e63c6c57866dc0552fe62f95bbf
tim48-robot <justintimothy303@gmail.com>
2025-09-16 12:17:20 +0700

    feat: creating product, product_list. also added id in models and style in base.html to get rid of integer spinner

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 # Create your models here.
@@ -10,6 +11,7 @@ class Product(models.Model):
         ('casual', 'Casual Wear and Merchandise')
     ]
     
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,10 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
+from main.forms import ProductForm
+
 # Create your views here.
+
 def show_main(request):
+    product_list = Product.objects.all()
+
     context = {
         'nama_aplikasi': 'football_project',
         'nama' : 'Justin Timothy Wirawan',
-        'kelas': 'PBP D'
+        'kelas': 'PBP D',
+        'product_list': product_list
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect("main:show_main")
+    
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
+
+
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    product.increment_views()
+
+    context = {
+        'product' : product
+    }
+    return render(request, "product_item.html", context)
+
+
+def show_xml(request):
+    product_list = Product.objects.all()
+    xml_data = serializers.serialize("xml", product_list)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    product_list = Product.objects.all()
+    json_data = serializers.serialize("json", product_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_xml_by_id(request, product_id):
+    try:
+        product_item = Product.objects.filter(pk=product_id)
+        xml_data = serializers.serialize("xml", product_item)
+        return HttpResponse(xml_data, content_type="application/xml")
+    except Product.DoesNotExist:
+        return HttpResponse(status=404)
+    
+def show_json_by_id(request, product_id):
+    try: 
+        product_item = Product.objects.filter(pk=product_id)
+        json_data = serializers.serialize("json", product_item)
+        return HttpResponse(json_data, content_type="application/json")
+    except Product.DoesnotExist:
+        return HttpResponse(status=404)
